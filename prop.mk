0ba1234f6097d7c61e55ecccf9a167c90e05bc2f
SyberHexen <SyberHexen@gmail.com>
2019-04-06 09:43:47 -0700

    mido: prop: Set persist.radio.aosp_usr_pref_sel=true
    * Fixes the bug in dual SIM device where preferred data selection didn't stick after reboot
    
    Change-Id: Ie47b06aa30f15d5b8c25b2378a99afd210a70683

--- prop.mk
+++ prop.mk
@@ -168,6 +168,7 @@ DEVICE_PROVISIONED=1 \
 persist.dbg.volte_avail_ovr=1 \
 persist.dbg.vt_avail_ovr=1 \
 persist.dbg.wfc_avail_ovr=1 \
+persist.radio.aosp_usr_pref_sel=true \
 persist.radio.multisim.config=dsds \
 persist.sys.fflag.override.settings_network_and_internet_v2=true \
 persist.vendor.radio.apm_sim_not_pwdn=1 \
